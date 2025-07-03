4704ce36680966b5c7601a3092ae3aac69566814
Gervasss <gervasiobrito10@gmail.com>
2025-07-03 15:54:03 -0300

    fix themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import AppRouter from './routes/AppRouter';
 import './App.css';
-import { ThemeProvider } from "./components/ThemeContext/ThemeContext"
+
 
 const App: React.FC = () => (
 
 
-<ThemeProvider>
-   {/* Envolvendo o app inteiro com o ThemeProvider */}
 
     <div className="App">
         <AppRouter />
     </div>
-  </ThemeProvider>
+
   
 
   
